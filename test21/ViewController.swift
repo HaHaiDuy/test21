08528b7b1a2e650e284aeb75903ebbddb7175e3e
duy <duy@duys-Mac.local>
2018-04-05 20:43:02 -0700

    Initial Commit
    duy

---
+++ test21/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test21
+//
+//  Created by duy on 4/5/18.
+//  Copyright © 2018 duy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
